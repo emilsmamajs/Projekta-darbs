bad8d8f3155520274f67163787eab18634dc6d07
emilsmamajs <144494064+emilsmamajs@users.noreply.github.com>
2023-12-28 15:32:25 +0000

    imports

--- result.py
+++ result.py
@@ -1,1 +1,10 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
+import time
+
+service = Service()
+option = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=option)
